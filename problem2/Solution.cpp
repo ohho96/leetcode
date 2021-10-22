d4a517f2bcbf6a23b604ae3663943a263f9b238e
ohho96 <ohho96@gmail.com>
2021-10-21 21:30:30 -0700

    started problem2

--- problem2/Solution.cpp
+++ problem2/Solution.cpp
@@ -15,6 +15,33 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
     }
+    bool validNum(ListNode* l){
+        int val = *l.val;
+        return (val > 10 || val < 0);
+    }
+
+    ListNode* arr2list(int* [100] arr){
+        int i = 1;
+        ListNode* ln = new ListNode(arr[0]);
+        while(arr[i] != NULL){
+            ListNode lt = new ListNode(arr[i]));
+            ln = ln->next;
+            i++;
+        }
+        return ln;
+    }
+
+    int* list2arr(ListNode* ln){
+        static int arr[100];
+        ListNode* l0 = ln;
+        int i = 0;
+        while(l0->next->val == NULL){
+            arr[i] = l0->val;
+            l0 = l0->next;
+            i++;
+        }
+        return arr;
+    }
 };
 
 int main(void) {
