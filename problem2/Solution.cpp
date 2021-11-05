3e8164f79868d0384ecf319e93367b9749bb9236
ohho96 <ohho96@gmail.com>
2021-11-05 14:55:23 -0700

    ohho: finished problem 2

--- problem2/Solution.cpp
+++ problem2/Solution.cpp
@@ -1,69 +1,94 @@
 #include <iostream>
 using namespace std;
 
+
 //Definition for singly-linked list.
-struct ListNode {
+/* struct ListNode {
     int val;
     ListNode *next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+}; */
+
+//invlid solution because causing signed integer oveflow on large number
+/* class Solution {
+public:
+    ListNode * addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int x1 = 0;
+        int x2 = 0;
+        int temp = 1;
+        int sum;
+
+        while(l1 != NULL){
+            x1 += temp*l1->val;
+            temp *= 10;
+            l1 = l1->next;
+        }
+
+        temp = 1;
+        while(l2 != NULL){
+            x2 += temp*l2->val;
+            temp *= 10;
+            l2 = l2->next;
+        }
+
+        sum = x1 + x2;
+        ListNode * head = new ListNode();
+        ListNode * tNode = head; 
+        if(sum == 0){
+            tNode->val = 0;
+        }
+        else {
+            while(sum > 0){
+                tNode->val = sum %10;
+                if(sum/10 != 0){
+                    tNode->next = new ListNode();
+                    tNode = tNode->next;
+                }
+                sum /= 10;
+            }
+        }
+        return head;
+    }
+}; */
 
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-    }
-    bool validNum(ListNode* l){
-        int val = *l.val;
-        return (val > 10 || val < 0);
-    }
+    ListNode * addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode * head = nullptr;
+        ListNode * temp = nullptr;
 
-    ListNode* arr2list(int* [100] arr){
-        int i = 1;
-        ListNode* ln = new ListNode(arr[0]);
-        while(arr[i] != NULL){
-            ListNode lt = new ListNode(arr[i]));
-            ln = ln->next;
-            i++;
-        }
-        return ln;
-    }
+        int carry = 0;
 
-    int* list2arr(ListNode* ln){
-        static int arr[100];
-        ListNode* l0 = ln;
-        int i = 0;
-        while(l0->next->val == NULL){
-            arr[i] = l0->val;
-            l0 = l0->next;
-            i++;
-        }
-        return arr;
-    }
+        while(l1 != nullptr || l2 != nullptr){
+            int sum = carry;
 
-    int* list2int(ListNode*ln){
-        static int num = 0;
-        int i = 0;
-        while(ln->next->val == NULL){
-            num = (num*10)+ln->val;
-            i++;
-            ln = ln->next;
-        }
-    }
+            if(l1 != nullptr){
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if(l2 != nullptr){
+                sum += l2->val;
+                l2 = l2->next;
+            }
 
-    ListNode* int2list(int* num){
-        static int num = 0;
-        int i = 0;
-        while(ln->next->val == NULL){
-            num = (num*10)+ln->val;
-            i++;
-            ln = ln->next;
+            ListNode * node = new ListNode(sum % 10);
+
+            carry = sum /10;
+
+            if (temp == nullptr){
+                temp = head = node;
+            }
+            else{
+                temp->next = node;
+                temp = temp->next;
+            }
         }
+
+        if(carry > 0){
+            temp->next = new ListNode(carry);
+        }
+        return head;
     }
 };
-
-int main(void) {
-
-}
